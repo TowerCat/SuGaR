10b8c5125ac081b15e92b26b3a7d41941c4ed34a
TowerCat <498652109@qq.com>
2024-03-09 12:20:04 +0800

    unique_str[0:10] 将3dgs的随机高斯字符串改为固定的

--- gaussian_splatting/train.py
+++ gaussian_splatting/train.py
@@ -137,8 +137,9 @@ def prepare_output_and_logger(args):
             unique_str=os.getenv('OAR_JOB_ID')
         else:
             unique_str = str(uuid.uuid4())
-        args.model_path = os.path.join("./output/", unique_str[0:10])
-        
+        # args.model_path = os.path.join("./output/", unique_str[0:10])
+        args.model_path = os.path.join("./output/", "originGsModel") # 方便批量自动化
+
     # Set up output folder
     print("Output folder: {}".format(args.model_path))
     os.makedirs(args.model_path, exist_ok = True)
